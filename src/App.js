581e87a17884a1e2559a00dbead0409d39373a0c
AminaKenessova <mikoshakenesova@gmail.com>
2020-06-01 01:16:47 +0600

    new commits

--- src/App.js
+++ src/App.js
@@ -34,8 +34,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+
+  loadUser = (data) =>{
+    this.setState({user:{
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+
+    }})
   }
 
 
@@ -62,13 +81,30 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onSubmit=()=>{
+  onPictureSubmit=()=>{
     this.setState({imageUrl: this.state.input});
     app.models
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if(response) {
+        fetch('http://localhost:300/image', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+      })
+    })
+        .then(response => response.json())
+        .then(count => {
+          this.setState({user: {
+            entries: count
+          }})
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err=> console.log(err));
         // there was an error
   }
@@ -103,8 +139,8 @@ class App extends Component {
 
       : (
             route ==='signin'
-          ? <Signin onRouteChange ={this.onRouteChange}/>
-          : <Register onRouteChange ={this.onRouteChange}/>  
+          ? <Signin loadUser ={this.loadUser} onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange ={this.onRouteChange}/>
         )      
     }
     </div>
